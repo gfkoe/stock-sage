e1fc6501ff60240fff9213fac8b141e3c78ca6ca
Gabe Koeb <gfkoeb@ncsu.edu>
2024-07-04 12:43:44 -0400

    Add stock input placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
-
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="min-h-screen p-24">
+      <div className="flex items-center">
+        <h1 className="text-xl ">Stock:</h1>
+        &nbsp;
+        <Input className="w-3/4" type="text" placeholder="Ex: NVDA" />
+        <Button type="submit">Add</Button>
+      </div>
+    </main>
   );
 }
