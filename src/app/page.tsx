cbdcffce484c20147a8e8c2ba42c05308d459eb1
Gabriel <gabrielkoeb@gmail.com>
2024-07-05 19:09:27 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import StockList from "@/components/StockList";
 export default function Home() {
   return (
