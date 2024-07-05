d9240e714d03c273ccb633c415ba25fbeacd11ae
Gabriel <gabrielkoeb@gmail.com>
2024-07-05 18:57:24 -0400

    Updated list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import StockList from "@/components/StockList";
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
-      <div className="flex items-center justify-evenly">
-        <label>Stock:</label>
-        &nbsp;
-        <Input
-          className="w-3/4"
-          id="stockname"
-          type="text"
-          placeholder="Ex: NVDA"
-        />
-        <Button type="submit">Add</Button>
-      </div>
+      <StockList />
     </main>
   );
 }
