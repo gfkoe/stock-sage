8c29336d0f79a0587e558a578c9c7286b84ee8c2
Gabe Koeb <gfkoeb@ncsu.edu>
2024-07-06 01:42:14 -0400

    add action

---
+++ src/actions/YahooFetch.ts
@@ -0,0 +1,15 @@
+"use server";
+import yahooFinance from "yahoo-finance2";
+
+export async function getStock(ticker: string) {
+  console.log(ticker);
+  const stock = await yahooFinance.quoteSummary(ticker);
+
+  const regularMarketPrice = stock?.price?.regularMarketPrice;
+  const symbol = stock?.price?.symbol;
+  console.log(regularMarketPrice, symbol);
+  return { regularMarketPrice, symbol };
+}
+
+export async function checkPrice(ticker: string) {}
+export default getStock;
