30688141e9dc43f26fc811cc93a5f0b608777161
Gabe Koeb <gfkoeb@ncsu.edu>
2024-07-07 19:23:33 -0400

    Working on login form

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/Icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <div>
+            <Label className="sr-only" htmlFor="email>">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
