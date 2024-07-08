175ab35d691a9f62ed1e50d4779cc50e40e06d08
Gabe Koeb <gfkoeb@ncsu.edu>
2024-07-07 22:23:52 -0400

    Working on login form

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -21,10 +21,10 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   }
 
   return (
-    <div>
+    <div className={cn("grid gap-6", className)} {...props}>
       <form onSubmit={onSubmit}>
-        <div>
-          <div>
+        <div className="grid gap2">
+          <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email>">
               Email
             </Label>
@@ -38,8 +38,32 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               disabled={isLoading}
             />
           </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Sign In with Email
+          </Button>
         </div>
       </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button variant="outline" type="button" disabled={isLoading}>
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.gitHub className="mr-2 h-4 w-4" />
+        )}{" "}
+        GitHub
+      </Button>
     </div>
   );
 }
