7031494664a0c30a56a3c42b1bd8672a24893822
Gabe Koeb <gfkoeb@ncsu.edu>
2024-07-20 03:05:32 -0400

    update and got authentication working

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -7,22 +7,34 @@ import { Icons } from "@/components/Icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { SignIn } from "@/actions/GithubOath";
+import { providerMap } from "../../auth";
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+const GitHubProvider = providerMap.find((provider) => provider.id === "github");
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
     setIsLoading(true);
-    setTimeout(() => {
+    try {
+      if (GitHubProvider) {
+        await SignIn({ provider: GitHubProvider });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
       setIsLoading(false);
-    }, 3000);
+    }
+    //setTimeout(() => {
+    //  setIsLoading(false);
+    //}, 3000);
   }
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
+      {/* <form onSubmit={onSubmit}>
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
@@ -45,23 +57,24 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             Sign In with Email
           </Button>
         </div>
-      </form>
+      </form> */}
       <div className="flex items-center">
-
         <div className="flex-1 border-t"></div>
         <div className="px-4 text-muted-foreground text-xs uppercase">
           Or continue with
         </div>
         <div className="flex-1 border-t"></div>
       </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
-        {isLoading ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.gitHub className="mr-2 h-4 w-4" />
-        )}{" "}
-        GitHub
-      </Button>
-    </div >
+      <form onSubmit={onSubmit}>
+        <Button variant="outline" type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <Icons.gitHub className="mr-2 h-4 w-4" />
+          )}{" "}
+          <span>Sign in with GitHub</span>
+        </Button>
+      </form>
+    </div>
   );
 }
